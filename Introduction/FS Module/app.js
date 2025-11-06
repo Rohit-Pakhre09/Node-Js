a82580f9ac40aabb329361c0b02ebf8c7c4eeb73
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-11-06 23:56:15 +0530

    Feat: Files and Folder added.

---
+++ Introduction/FS Module/app.js
@@ -0,0 +1,5 @@
+const { areaOfCircle, areaOfSquare, areaOfRectangle } = require('./math');
+
+console.log(areaOfCircle(5));  
+console.log(areaOfSquare(4));
+console.log(areaOfRectangle(4, 6));
---
+++ Introduction/FS Module/math.js
@@ -0,0 +1,17 @@
+const areaOfCircle = (r) => {
+    return 3.14 * r * r;
+};
+
+const areaOfSquare = (s) => {
+    return s * s;
+};
+
+const areaOfRectangle = (l, b) => {
+    return l * b;
+};
+
+module.exports = {
+    areaOfCircle,
+    areaOfSquare,
+    areaOfRectangle
+};
